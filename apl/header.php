5be892c74a5e291e47cd6cdb2c48c3557766d067
Harriet <harriet@littlereddiesel.com>
2018-10-29 15:01:02 +0000

    APL download link added to header

--- apl/header.php
+++ apl/header.php
@@ -30,13 +30,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 <header>
 	<nav class="navbar navbar-expand-lg navbar-dark">
 	  <a class="navbar-brand" href="/"><img class="primary-logo" src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/apl-logo.jpg" /></a>
-	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+		<!--?php	$downloadimg = get_field('lp_download'); if( !empty($downloadimg) ): ?>
+			<a href="<!--?php the_field('lp_download'); ?>/assets/ArcadiaSOP.pdf" download class="download-link"><img src="<!--?php echo get_stylesheet_directory_uri(); ?>/assets/img/download.png" /></a>
+		<!--?php endif; ? -->
+
+		<a href="<?php echo get_stylesheet_directory_uri(); ?>/assets/ArcadiaSOP.pdf" download class="download-link"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/download.png" /></a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
 
 	  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
 			<?php wp_nav_menu( array( 'menu'=> 'nav-primary', 'container'=> false, 'menu_class'=> 'navbar-nav mr-auto', 'menu_id'=> 'primaryNavTop' ) );  ?>
-
 			<form role="search" method="get" id="searchform" class="searchform form-inline my-2 my-lg-0"  action="<?php bloginfo('home'); ?>/">
 				<input class="form-control mr-sm-2" placeholder="Search site..."  type="text" value="<?php get_search_query(); ?>" name="s" id="s" />
 				<input type="hidden" name="search-type" value="normal" />
